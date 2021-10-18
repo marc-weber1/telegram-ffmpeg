6005d4695ad0f249fd4d31ad8d3c9872b7a68012
facade <mwiloveicecream565@gmail.com>
2021-10-18 22:48:59 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const { Telegraf, Markup } = require('telegraf')
+
+const token = process.env.BOT_TOKEN
+if (token === undefined) {
+  throw new Error('BOT_TOKEN must be provided!')
+}
+
+
+const bot = new Telegraf(token)
+
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
