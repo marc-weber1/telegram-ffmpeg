9822ecf5f08722f4673a3ae2b9061a3a6fd53d87
facade <mwiloveicecream565@gmail.com>
2021-10-22 02:59:02 +0000

    Video downloading now working

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const { Telegraf, Markup } = require('telegraf')
+const axios = require('axios')
+const fs = require('fs')
 
 const token = process.env.BOT_TOKEN
 if (token === undefined) {
@@ -8,8 +10,29 @@ if (token === undefined) {
 
 const bot = new Telegraf(token)
 
+bot.start((ctx) => ctx.reply('Hi! Forward a video to me, or paste a youtube link.'))
+bot.on("message", (ctx, next) => {
+    ctx.telegram.getFileLink(ctx.message.video.file_id)
+      .then( url => {
+        console.log(url.href)
+        return axios({ url: url.href , responseType: 'stream'})
+      })
+      .then( response => {
+        response.data.pipe(fs.createWriteStream(`/var/www/telegramwebhook/public/videos/${ctx.update.message.from.id}.mp4`))
+      })
+      .catch(error => {
+        ctx.reply("Error: " + error)
+      })
+      .catch(error => { // ctx.reply failed
+        console.log("Telegram context failure: ", error)
+      })
+    
+} )
 
 bot.launch()
+//bot.telegram.setWebhook("http://...")
+//bot.startWebhook('/var/www/telegramwebhook', null, 25003)
+
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
