026fd48d12da21e8d5abf501cb3871d925ef1dd3
facade <mwiloveicecream565@gmail.com>
2021-10-26 07:14:53 +0000

    Converting to mp3 now functions properly

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { Telegraf, Context, Markup, session } from 'telegraf'
 import { Message } from 'typegram'
 import axios from 'axios'
 import * as fs from 'fs'
-var mime = require('mime-types')
+var fsPromises = require('fs').promises
+const mime = require('mime-types')
+const exec = require('child_process').exec;
 
 //const MAX_FILE_SIZE = 50 << 20  // << 20 converts MB to bytes
 
 const AUDIO_TYPES = [
-  "mp3", "vorbis", "opus", "flac", "wav"
+  "mp3", "flac", "wav"
 ]
 
 interface AudioContainer{
@@ -20,6 +22,7 @@ interface AudioContainer{
 interface SessionData{
   lastMedia?: AudioContainer
   audioOutType?: string
+  audioOutCompression?: number
 }
 
 interface MRContext extends Context {
@@ -38,13 +41,42 @@ if (token === undefined) {
 function downloadFile(url : URL, path : string){
   return axios({ url: url.href , responseType: 'stream'})
     .then( response => {
-      return (response.data as any).pipe(fs.createWriteStream(`/var/www/telegramwebhook/temp-downloads/${path}`))
+      return (response.data as any).pipe(fs.createWriteStream(`${process.env.MAIN_PATH}/temp-downloads/${path}`))
         //.on('finish', () => /* File is saved. */)
         //.on('error', e => /* An error has occured */)
     })
     .catch(error => {
       console.log("Could not save file: ", error)
     })
+}
+
+
+ function execShellCommand(cmd) {
+  return new Promise((resolve, reject) => {
+   exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+     console.warn(error);
+    }
+    resolve(stdout? stdout : stderr);
+   });
+  });
+ }
+
+// Assumes ctx.session.lastMedia and audioOutType exist, and possibly audioOutCompression if the format needs it
+async function convertFile(ctx: MRContext){ // TODO
+  if(ctx.session.audioOutType === "mp3"){
+    // Download and process
+    return execShellCommand(`ffmpeg -i "${ctx.session.lastMedia.url}" -vn -c:a libmp3lame -q ${ctx.session.audioOutCompression} ${process.env.MAIN_PATH}/temp-downloads/${ctx.session.lastMedia.file_unique_id}.mp3`)
+      .then(() => { // Send back to the user
+        return ctx.replyWithAudio({source: `${process.env.MAIN_PATH}/temp-downloads/${ctx.session.lastMedia.file_unique_id}.mp3`})
+      })
+      .then(() => {  // Delete the file after
+        return fsPromises.unlink(`${process.env.MAIN_PATH}/temp-downloads/${ctx.session.lastMedia.file_unique_id}.mp3`)
+      })
+  }
+  else if(ctx.session.audioOutType === "wav"){
+    // ...
+  }
 }
 
 
@@ -70,7 +102,7 @@ bot.on("message", (ctx: MRContext, next) => {
       .then(url => {
         ctx.session.lastMedia = {url: url, file_unique_id: audio.file_unique_id, mime_type: audio.mime_type, file_size: audio.file_size}
         ctx.reply("What audio format to convert to?", Markup
-          .keyboard(['mp3','ogg','opus','flac','wav'])
+          .keyboard(AUDIO_TYPES)
           .oneTime()
           .resize()
           )
@@ -121,28 +153,45 @@ bot.on("message", (ctx: MRContext, next) => {
     let text = (ctx.message as Message.TextMessage).text
 
     if( ctx.session.audioOutType ){
+      // Try to get a bitrate number out of 'text'
+      let compression: number = Number(text)
 
+      if( isNaN(compression) ){
+        ctx.reply("Requires a number between 0 and 10.")
+      }
+      else{
+        // Ready to convert
+        ctx.session.audioOutCompression = compression
+        convertFile(ctx).then(_ => {
+          // Reset the bot
+          ctx.session.lastMedia = undefined // Change this to the media just uploaded next
+          ctx.session.audioOutType = undefined
+        })
+        
+      }
     }
     else if( ctx.session.lastMedia ){ //What to convert to?
 
       if(text === "done"){
-        ctx.reply("OK. Send more media when you're ready to convert again.")
-        
         // Reset the bot
         ctx.session.lastMedia = undefined
         ctx.session.audioOutType = undefined
+
+        ctx.reply("OK. Send more media when you're ready to convert again.")
       }
       else if(text === "wav" || text == "flac"){
-        //Ready to convert
-        ctx.reply("Command: convert " + ctx.session.lastMedia.url.toString() + " to " + text )
-
-        // Reset the bot
-        ctx.session.lastMedia = undefined // Change this to the media just uploaded next
-        ctx.session.audioOutType = undefined
+        ctx.session.audioOutType = text
+        // Ready to convert
+        convertFile(ctx).then(_ => {
+          // Reset the bot
+          ctx.session.lastMedia = undefined // Change this to the media just uploaded next
+          ctx.session.audioOutType = undefined
+        })
       }
-      else if(text === "mp3" || text === "vorbis" || text === "opus"){
+      else if(text === "mp3"){
         //We still need a bitrate
-        ctx.reply("Command: convert " + ctx.session.lastMedia.url.toString() + " to " + text )
+        ctx.session.audioOutType = text
+        ctx.reply("Give the amount of compression to apply from 0 (best) to 10 (worst).")
       }
       else{
         ctx.reply("Please pick one of the options: " + AUDIO_TYPES.join(", "), Markup
@@ -151,8 +200,6 @@ bot.on("message", (ctx: MRContext, next) => {
           .resize()
           )
       }
-
-      //return downloadFile(url, `videos/${ctx.message.video.file_id}.${mime.extension(ctx.message.video.mime_type)}`)
     }
     else{
       ctx.reply("Start by sending audio or video.")
@@ -168,7 +215,7 @@ bot.on("message", (ctx: MRContext, next) => {
 } )
 
 bot.launch()
-  .then(fulfilled =>{
+  .then(fulfilled => {
     console.log("Bot running.")
   })
 //bot.telegram.setWebhook("http://...")
